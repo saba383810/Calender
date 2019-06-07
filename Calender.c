4df1d33f8a16fc104857e01b45ad30d499271f10
saba383810 <saba383810@gmail.com>
2019-06-08 01:02:24 +0900

    update Calender.c

--- Calender.c
+++ Calender.c
@@ -2,21 +2,28 @@
 #include<stdlib.h>
 
 int Check_Leapyear(int);
+int End(void);
 
-int main (){
+int main(){
 
 	int year,month,cnt,week_cnt;
 	int month_date[12] = {31,28,31,30,31,30,31,31,30,31,30,31};
-	int total = 0;
+	int total = 1;
+	int oko_cnt = 0;
 	printf("カレンダーを出力します。\n");
 	do{
-		printf("年(1以上)を入力してください。>>");
+		printf("年(西暦)を入力してください。>>");
 		scanf("%d",&year);
-	}while(year<1);
-
+		if(oko_cnt >= 3) End();
+		oko_cnt++;
+	}while(year<1 || year > 12019);
+	oko_cnt = 0;
 	do{
 		printf("月(1~12)を入力してください。>>");
 		scanf("%d",&month);
+		if(oko_cnt >= 3) End();
+		oko_cnt++;
+
 	}while(month>12 || month<1);
 
 	// 閏年計算
@@ -30,6 +37,7 @@ int main (){
 	// カレンダー
 	printf("\n\n\t%d年\n%2d月\n",year,month);
 	printf("  日 月 火 水 木 金 土\n");
+	printf("----------------------\n");
 	for(cnt = 1,week_cnt = 0;cnt <= total%7;cnt++,week_cnt++) printf("   ");
 	
 	for(cnt = 1;cnt <= month_date[month-1];cnt++,week_cnt++){
@@ -37,14 +45,18 @@ int main (){
 		
 		printf("%3d",cnt);
 	}
+	printf("\n");
 	return 0;
 }
 
 // 閏年なら”1”を返す関数
 int Check_Leapyear(int year){
-	if(year%400==0||(year%4==0 && year%100 != 0)){
+	if(year%400==0||(year%4==0 && year%100 != 0))
 		return 1;
-	}else{
+	else
 		return 0;
-	}
+}
+int End(void){
+	printf("\n+------------+\n|おこだよ！！|\n+------------+\n( ・∇・)\n");
+	exit(0);
 }
